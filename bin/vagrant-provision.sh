5b60a32af4f47f6c4c290007942a3e8b87f774fd
Jason Brown <jason@silex-consulting.com>
2014-10-15 13:08:02 +0100

    Update vagrant-provision.sh with new puppet module directory.

--- bin/vagrant-provision.sh
+++ bin/vagrant-provision.sh
@@ -9,14 +9,14 @@ fi
 
 sudo apt-get update
 
-#Temporary - need to add these to the base box? Comment these out after the first run.
+#Temporary - need to add these to the base box?
 sudo gem install hiera
 sudo gem install hiera-eyaml
 sudo apt-get install ruby1.9.1-dev
 
 #Update puppet module dependencies using librarian-puppet
 sudo gem install librarian-puppet
-cd /vagrant/puppet-modules
+cd /vagrant/modules
 librarian-puppet update
 
 if [ ! -f /vagrant/keys/private_key.pkcs7.pem ];
@@ -26,6 +26,6 @@ then
 fi
 
 sudo rm -rf /etc/puppet/modules
-ln -sf /vagrant/puppet-modules /etc/puppet/modules
+ln -sf /vagrant/modules /etc/puppet/modules
 ln -sf /vagrant/hiera.yaml /etc/puppet/
-ln -sf /vagrant/puppet-modules/beluga/files/hieradata /etc/
+ln -sf /vagrant/modules/beluga/files/hieradata /etc/
