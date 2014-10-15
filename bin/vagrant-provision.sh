b6416bcb171231d83d26ba6a1c0d95272e39826f
Jason Brown <jason@silex-consulting.com>
2014-10-15 12:37:17 +0100

    Use librarian puppet for module dependencies.

--- bin/vagrant-provision.sh
+++ bin/vagrant-provision.sh
@@ -6,37 +6,21 @@ then
   echo 'Found local vagrant.pp'
 fi
 
-#Update Apt
+#Update Apt&Get Git!
 sudo apt-get update
 
 #Temporary - need to add these to the base box? Comment these out after the first run.
 sudo gem install hiera
 sudo gem install hiera-eyaml
 
-#Currently librarian-puppet is not working out of the box for ruby 1.9.1
-#see below for required modules
-#sudo gem install librarian-puppet
 #Update puppet module dependencies using librarian-puppet
-#librarian-puppet update
-
-#Do it yourself!
+sudo gem install librarian-puppet
 cd /vagrant/puppet-modules
-git clone git@github.com:SilexConsulting/puppetlabs-apache.git apache
-git clone git@github.com:SilexConsulting/puppetlabs-apt
-git clone git@github.com:SilexConsulting/puppet-beluga.git beluga
-git clone git@github.com:SilexConsulting/puppetlabs-composer.git
-git clone git@github.com:SilexConsulting/puppetlabs-concat.git
-git clone git@github.com:SilexConsulting/puppet-jenkins.git jenkins
-git clone git@github.com:SilexConsulting/puppetlabs-mysql.git
-git clone git@github.com:SilexConsulting/puppetlabs-mysql.git
-git clone git@github.com:SilexConsulting/puppet-postfix
-git clone git@github.com:SilexConsulting/puppetlabs-stdlib.git
-git clone git@github.com:SilexConsulting/puppet-wget.git
+librarian-puppet update
 
 if [ ! -f /vagrant/keys/private_key.pkcs7.pem ];
 then
-    mkdir /vagrant/keys
-    cd /vagrant/keys
+    cd /vagrant
     eyaml  createkeys
 fi
 
