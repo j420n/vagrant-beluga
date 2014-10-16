c2809968dea7c768abb2088e3a81739175178dbc
Jason Brown <jason@silex-consulting.com>
2014-10-16 21:49:16 +0100

    Add two possible options to install librarian puppet +\n.

--- bin/vagrant-provision.sh
+++ bin/vagrant-provision.sh
@@ -12,12 +12,19 @@ sudo apt-get update
 #Temporary - need to add these to the base box?
 sudo gem install hiera
 sudo gem install hiera-eyaml
-sudo apt-get install ruby1.9.1-dev
 
-#Update puppet module dependencies using librarian-puppet
-sudo gem install librarian-puppet
+#OPTION 1
+#Update Puppetfile dependencies using librarian-puppet from apt-get.
+#sudo apt-get install git-core -y
+#sudo apt-get install librarian-puppet -y
+
+#OPTION 2
+#Update puppet module dependencies using gem install librarian-puppet.
+sudo apt-get install ruby1.9.1-dev
+sudo gem install librarian-puppet --verbose
+
 cd /vagrant/modules
-librarian-puppet update
+librarian-puppet update --verbose
 
 if [ ! -f /vagrant/keys/private_key.pkcs7.pem ];
 then
