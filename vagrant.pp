7dacd50c71900d51a20818bf5d9fb786b98dd970
Jason Brown <jason@silex-consulting.com>
2014-10-23 14:58:29 +0100

    Add beluga user and 2 drupal website to vagrant.pp

--- vagrant.pp
+++ vagrant.pp
@@ -1,3 +1,21 @@
 #INCLUDE CLASSES DECLARED IN HIERADATA
 #Look at beluga/files/hieradata/common.yaml
 hiera_include("classes")
+
+#DECLARE DRUPAL SITE NAMES
+::beluga::drupal_site {'beluga' : }
+::beluga::drupal_site {'silex' : }
+
+#DEFAULT DRUPAL SITE OWNER
+#TO OVERRIDE THIS SET "::beluga::drupal_site::<NAME-OF-SITE>::site_owner: <YOUR-NAME>" IN COMMON.YAML
+beluga::user {'beluga':
+   uid => 5001,
+   ssh_key => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ',
+   key_type => 'ssh-rsa'
+}
+#IF YOU WANT TO CHANGE THIS CREATE A NEW USER BELOW
+#beluga::user {'<NEW-USER-NAME>':
+#  uid => 5005,
+#  ssh_key => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ',
+#  key_type => 'ssh-rsa'
+#}
