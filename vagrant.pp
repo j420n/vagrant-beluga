bf404aa510808cb57dd9f01b9070fb5de04f6307
Jason Brown <jason@silex-consulting.com>
2015-05-08 15:47:33 +0100

    Update beluga UID and site name.

--- vagrant.pp
+++ vagrant.pp
@@ -1,15 +1,23 @@
 #INCLUDE CLASSES DECLARED IN HIERADATA
 #Look at beluga/files/hieradata/common.yaml
 hiera_include("classes")
+stage { 'pre':
+  before => Stage["main"],
+}
+class {'puppetdb':
+stage => 'pre',
+}
 
 #DECLARE DRUPAL SITE NAMES
-::beluga::drupal_site {'beluga' : }
-::beluga::drupal_site {'silex' : }
+beluga::drupal_site {'silex' : }
+
+#DECLARE CUSTOM SITE NAMES
+#beluga::custom_site {'silex' : }
 
 #DEFAULT DRUPAL SITE OWNER
 #TO OVERRIDE THIS SET "::beluga::drupal_site::<NAME-OF-SITE>::site_owner: <YOUR-NAME>" IN COMMON.YAML
 beluga::user {'beluga':
-   uid => 5001,
+   uid => 5002,
    ssh_key => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ',
    key_type => 'ssh-rsa'
 }
